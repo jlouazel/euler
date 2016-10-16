beebebbec2bd096c5c3ed3aa67122e488e3291c0
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2016-10-16 17:41:37 +0200

    Resolve problem 1

--- problem_1/src/main.rs
+++ problem_1/src/main.rs
@@ -1,3 +1,15 @@
+fn sum_of_multiples() -> i32 {
+    let mut sum = 0;
+
+    for x in 1..1000 {
+        if x % 3 == 0 || x % 5 == 0 {
+            sum += x;
+        }
+    }
+
+    return sum;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", sum_of_multiples());
 }
